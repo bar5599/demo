3c64ff165583cc2bab5625425dbf799ba62a7889
bar5599 <bar1258337@gmail.com>
2021-10-14 19:40:28 +0300

    Initial commit

---
+++ src/main/java/com/barisoz/demo/ProjectApplication.java
@@ -0,0 +1,17 @@
+package com.barisoz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
